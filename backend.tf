4db3986b0378488649c2b895efeda9d2281414c3
Abdullah Rizwan <abdullahrn8@gmail.com>
2024-07-10 16:48:38 +0500

    fixed dynamodb

--- backend.tf
+++ backend.tf
@@ -3,7 +3,6 @@ terraform {
     bucket = "terraform-tfstate-bucket-905418336711"
     region = "us-east-1"
     key = "terraform.tfstate"
-    dynamodb_table = "data_onenets_tf_lockid"
     encrypt= false
   }
 }
